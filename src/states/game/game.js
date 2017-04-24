2123df96b2e4edaeb5f53920c2b507d512261949
Jakub Freisler <jakub.freisler@wingu.de>
2017-04-24 15:42:54 +0200

    Added music

--- src/states/game/game.js
+++ src/states/game/game.js
@@ -1,12 +1,12 @@
 define(function() {
 
-    DEFAULT_LEVEL_DATA = {
-        level: 0,
-        score: 0
-    };
+    var DEFAULT_LEVEL_DATA = {
+            level: 0,
+            score: 0
+        },
+        STEP_SIZE = 4;
 
     function init (levelData) {
-        console.log(levelData);
         this.score     = 0;
         this.levelData = levelData === undefined
                         ? DEFAULT_LEVEL_DATA
@@ -14,42 +14,53 @@ define(function() {
 
         this.score += this.levelData.score;
         this.levelData.score = 0;
-
-        this.nextLevel();
     }
 
     function preload () {
         this.logo = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'phaserLogo');
+        this.game.load.audio('music', ['https://examples.phaser.io/assets/audio/bodenstaendig_2000_in_rock_4bit.mp3']);
         this.logo.anchor.setTo(0.5);
     }
 
     function create () {
+        this.mainLoop = this.game.add.audio('music');
+
+        this.mainLoop.play();
+
         this.game.stage.backgroundColor = "#7e8652";
         this.escKey = this.game.input.keyboard
           .addKey(Phaser.Keyboard.ESC);
 
+        this.arrowLeft = this.game.input.keyboard
+          .addKey(Phaser.Keyboard.LEFT);
+
+        this.arrowRight = this.game.input.keyboard
+          .addKey(Phaser.Keyboard.RIGHT);
+
         this.escKey.onDown.add(this.endGame, this);
+        this.arrowLeft.onDown.add(this.goLeft, this);
+        this.arrowRight.onDown.add(this.goRight, this);
     }
 
 
     function render () {
-
+        this.game.debug.soundInfo(this.mainLoop, 20, 32);
     }
 
     function update () {
+    
+    }
 
+    function goRight () {
+        this.logo.x += STEP_SIZE;
+    }
+
+    function goLeft () {
+        this.logo.x -= STEP_SIZE;
     }
 
     function endGame () {
         this.game.state.start('menu', true, false, this.levelData);
-    }
-
-    function nextLevel () {
-        this.levelData.level++;
-
-        this.score += this.levelData.score;
-
-        this.game.state.start('level', true, false, this.levelData);
     }
 
 
@@ -60,6 +71,7 @@ define(function() {
         update   : update,
         render   : render,
         endGame  : endGame,
-        nextLevel: nextLevel
+        goLeft   : goLeft,
+        goRight  : goRight
     };
 });
