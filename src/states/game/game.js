b0ca097ad8d768401fb360e36abb97fe238913c3
Nevaan <pawel.losek@best.krakow.pl>
2017-04-24 14:00:14 +0200

    step2

--- src/states/game/game.js
+++ src/states/game/game.js
@@ -1,14 +1,26 @@
-define(function() {
+define(['Phaser'] , function(Phaser) {
+
+    var circle, ellipse, polygon, rectangle, roundedRect;
+
 
     function preload() {
-        this.logo = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'phaserLogo');
-        this.logo.anchor.setTo(0.5);
     }
 
     function create() {
+        circle = this.add.graphics(this.game.world.centerX, this.game.world.centerY);
+        ellipse = this.add.graphics(this.game.world.centerX, this.game.world.centerY);
+        polygon = this.add.graphics(this.game.world.centerX, this.game.world.centerY);
+        rectangle = this.add.graphics(this.game.world.centerX, this.game.world.centerY);
+        roundedRect = this.add.graphics(this.game.world.centerX, this.game.world.centerY);
+
+        createCircle(50, 50, 20);
+        createEllipse(-20, -20, 70, 30)
+        createPolygon([ new Phaser.Point(200, 100), new Phaser.Point(350, 100), new Phaser.Point(375, 200), new Phaser.Point(150, 200) ]);
+        createRectangle(-100, -200, 50,150);
+        createRoundedRectangle(100,-200, 120,180, 40);
+
         this.game.stage.backgroundColor = "#7e8652";
     }
-
 
     function render() {
     }
@@ -16,6 +28,37 @@ define(function() {
     function update() {
     }
 
+    function createCircle(x,y,d) {
+        circle.beginFill(0x434986);
+        circle.drawCircle(x,y,d);
+        circle.endFill();
+    }
+
+    function createEllipse(x,y,width,height) {
+        ellipse.beginFill(0x248617);
+        ellipse.drawEllipse(x,y,width,height);
+        ellipse.endFill();
+    }
+
+    function createPolygon(path) {
+        var poly = new Phaser.Polygon();
+        poly.setTo(path);
+        ellipse.beginFill(0x105286);
+        ellipse.drawPolygon(poly);
+        ellipse.endFill();
+    }
+
+    function createRectangle(x,y,width,height) {
+        rectangle.beginFill(0x860B1A);
+        rectangle.drawRect(x,y,width,height);
+        rectangle.endFill();
+    }
+
+    function createRoundedRectangle(x,y,width,height, radius) {
+        roundedRect.beginFill();
+        roundedRect.drawRoundedRect(x,y,width,height, radius);
+        roundedRect.endFill();
+    }
 
     return {
         create: create,
--- src/states/menu/menu.js
+++ src/states/menu/menu.js
@@ -4,23 +4,22 @@ define(function() {
         this.game.stage.backgroundColor = "#366e9f";
         this.logo = this.add.sprite(this.game.world.centerX, this.game.world.centerY - 75, 'phaserLogo');
         this.logo.anchor.setTo(0.5);
-        //this.game.state.start('game');
     }
 
     function create() {
-        var graphics = this.add.graphics(this.game.world.centerX, this.game.world.centerY);
-        graphics.beginFill(0xE5FF60);
-        graphics.drawRoundedRect(-75,150,150,50, 20);
-        graphics.endFill();
+        var menuButton = this.add.graphics(this.game.world.centerX, this.game.world.centerY);
+        menuButton.beginFill(0xE5FF60);
+        menuButton.drawRoundedRect(-75,150,150,50, 20);
+        menuButton.endFill();
 
         var style = { font: "25px Arial", fill: "#000000", align: "center" };
         var text = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 175, "Start game", style);
         text.anchor.set(0.5);
 
-        graphics.inputEnabled = true;
-        graphics.input.useHandCursor = true;
+        menuButton.inputEnabled = true;
+        menuButton.input.useHandCursor = true;
 
-        graphics.events.onInputDown.add(function() {
+        menuButton.events.onInputDown.add(function() {
             this.game.state.start('game');
         }, this);
     }
